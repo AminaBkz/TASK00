3af4471361864e875adcfebec1c5592b66b49a43
Amina bkz <110788347+AminaBkz@users.noreply.github.com>
2025-04-21 20:33:50 +0100

    Updated task00.py

--- task00.py
+++ task00.py
@@ -1,4 +1,4 @@
-import os
+'''import os
 
 def rename_images_in_folder(folder_path):
     files = os.listdir(folder_path)
@@ -13,4 +13,21 @@ def rename_images_in_folder(folder_path):
         print(f"Renamed {filename} to {new_filename}")
 
 folder_path = r"C:\Users\bouak\OneDrive\Desktop\idk but ye\horse"
-rename_images_in_folder(folder_path)
+rename_images_in_folder(folder_path)'''
+
+# updated script
+
+import os
+
+def rename(folder_path):
+    for idx, filename in enumerate(os.listdir(folder_path)):
+        src = f"{folder_path}\\{filename}"
+        dst = f"{folder_path}\\imaage-{idx:02d}.png"
+        os.rename(src, dst)
+        print(f"Renamed {src} to {dst}")
+
+root_folder = r"C:\Users\bouak\OneDrive\Desktop\Dataloader Dataset"
+
+for subfolder in os.listdir(root_folder):
+    subfolder_path = f"{root_folder}\\{subfolder}"
+    rename(subfolder_path)
