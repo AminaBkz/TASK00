34e3ef17ac98c33c0c1422d766e05fab90d3f680
Amina bkz <110788347+AminaBkz@users.noreply.github.com>
2025-04-22 20:18:44 +0100

    Update task00.py

--- task00.py
+++ task00.py
@@ -1,22 +1,3 @@
-'''import os
-
-def rename_images_in_folder(folder_path):
-    files = os.listdir(folder_path)
-
-    for i, filename in enumerate(files):
-      
-        old_path = os.path.join(folder_path, filename)
-        new_filename = f"image-{i:02d}.png"
-        new_path = os.path.join(folder_path, new_filename)
-
-        os.rename(old_path, new_path)
-        print(f"Renamed {filename} to {new_filename}")
-
-folder_path = r"C:\Users\bouak\OneDrive\Desktop\idk but ye\horse"
-rename_images_in_folder(folder_path)'''
-
-# updated script
-
 import os
 
 
@@ -34,4 +15,4 @@ def rename_images(root_dir, ext='png'):
                 os.rename(old_path, new_path)
                 print(f"Renamed: {old_path} -> {new_path}")
 
-rename_images(r"C:\Users\bouak\OneDrive\Desktop\Dataloader Dataset", 'png')
+rename_images("Dataloader Dataset", 'png')
